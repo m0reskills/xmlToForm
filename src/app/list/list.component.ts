315724dae844280f0b741569a8ce895c9d3294e1
m0reskills <morelucky84@gmail.com>
2019-12-22 09:32:45 +0300

    fix with 'greater than' entity in checkOperators

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -57,7 +57,7 @@ export class ListComponent implements OnInit {
         operator = '&lt;';
         break;
       case '>' :
-        operator = '&lt;';
+        operator = '&gt;';
         break;
     }
     return operator;
